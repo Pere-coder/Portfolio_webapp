96f59adeb93d367346448bb5a339db9dfe3129e0
Pere <gpere800@gmail.com>
2023-02-24 11:04:40 -0800

    Changes to be committed: 	modified:   db.sqlite3 	modified:   portfolioapp/__pycache__/views.cpython-310.pyc 	modified:   portfolioapp/templates/portfolioapp/index.html 	modified:   portfolioapp/views.py

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import photos, contact
 
+
 # Create your views here.
 def viewpage(request):
-    photo = photos.objects.all()
-    ph = {'photos': photos, 'contact': contact}
-    return render(request, 'portfolioapp/index.html', ph)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        
+        new_message = contact(name=name, email=email, message=message)
+        new_message.save()
+    return render(request, 'portfolioapp/index.html')
